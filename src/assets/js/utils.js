37e84742f726348ab069b054740ddd5ee0ec5860
“immanuel-savvy” <immanuelsavvy@gmail.com>
2023-02-12 08:49:19 +0100

    1676188159452941189

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,5 +1,54 @@
 const DEV = true;
 
-const client_domain = DEV ? "http://localhost:3000" : "";
+const hostname = DEV ? "http://localhost" : "https://qna.giitafrica.com";
 
-export { client_domain };
+const client_domain = DEV ? `${hostname}:3000` : "";
+
+const domain = DEV ? `${hostname}:1448` : `https://qna.giitafrica.com`;
+
+const month_index = new Object({
+  0: "jan",
+  1: "feb",
+  2: "mar",
+  3: "apr",
+  4: "may",
+  5: "jun",
+  6: "jul",
+  7: "aug",
+  8: "sep",
+  9: "oct",
+  10: "nov",
+  11: "dec",
+});
+
+const dow_index = new Object({
+  1: "monday",
+  2: "tuesday",
+  3: "wednesday",
+  4: "thursday",
+  5: "friday",
+  6: "saturday",
+  7: "sunday",
+});
+
+const dow_index_inverse = new Object({
+  monday: 1,
+  tuesday: 2,
+  wednesday: 3,
+  thursday: 4,
+  friday: 5,
+  saturday: 6,
+  sunday: 7,
+});
+
+const organisation_name = "QNA";
+
+export {
+  client_domain,
+  month_index,
+  dow_index,
+  dow_index_inverse,
+  organisation_name,
+  domain,
+  hostname,
+};
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { email_regex } from "../assets/js/functions";
+import { post_request } from "../assets/js/services";
+import { Loggeduser } from "../contexts";
 import Footer from "../sections/footer";
 import Header from "../sections/header";
 
@@ -7,54 +10,123 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { password: "" };
   }
 
-  render() {
+  is_set = () => {
+    let { email, password, confirm_password, confirm } = this.state;
+
     return (
-      <>
-        <Header />
+      email_regex.test(email) &&
+      password.trim() &&
+      password === confirm_password &&
+      confirm
+    );
+  };
 
-        <section class="form_container">
-          <h2>Free Registration</h2>
+  signup = async () => {
+    let { email, password, loading } = this.state;
+    if (loading) return;
 
-          <form action="#">
-            <div class="input_container">
-              <p>Email*</p>
-              <input type="email" placeholder="E-mail" />
-            </div>
+    this.setState({ loading: true });
 
-            <div class="input_container">
-              <p>Password*</p>
-              <input type="password" placeholder="Password" />
-            </div>
+    let user = { email, password };
+    let res = await post_request("signup", user);
+    if (!res._id) return this.setState({ message: res, loading: false });
 
-            <div class="input_container">
-              <p>Confirm Password*</p>
-              <input type="password" placeholder="Password" />
-            </div>
+    user._id = res._id;
+    user.created = res.created;
+    delete user.password;
 
-            <div class="form_extra">
-              <div class="radio_container">
-                <input type="radio" />
+    document.getElementById("click_verify").click();
+  };
+
+  render() {
+    let { email } = this.state;
+
+    return (
+      <Loggeduser.Consumer>
+        {({ login }) => {
+          this.login = login;
+          return (
+            <>
+              <Header />
+
+              <section class="form_container">
+                <h2>Free Registration</h2>
+
+                <form action="#">
+                  <div class="input_container">
+                    <p>Email*</p>
+                    <input
+                      onChange={({ target }) =>
+                        this.setState({ email: target.value })
+                      }
+                      type="email"
+                      placeholder="E-mail"
+                    />
+                  </div>
+
+                  <div class="input_container">
+                    <p>Password*</p>
+                    <input
+                      onChange={({ target }) =>
+                        this.setState({ password: target.value })
+                      }
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </div>
+
+                  <div class="input_container">
+                    <p>Confirm Password*</p>
+                    <input
+                      onChange={({ target }) =>
+                        this.setState({ confirm_password: target.value })
+                      }
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </div>
+
+                  <div class="form_extra">
+                    <div class="radio_container">
+                      <label for="confirm">
+                        <input
+                          type="checkbox"
+                          onChange={({ target }) =>
+                            this.setState({ confirm: target.checked })
+                          }
+                          id="confirm"
+                        />
+                        &nbsp;&nbsp;I have read and accept to Exere{" "}
+                        <a href="#">Exere Terms & Conditions</a>
+                      </label>
+                    </div>
+                  </div>
+
+                  <br />
+                  <button
+                    onClick={this.signup}
+                    disabled={!this.is_set()}
+                    type="submit"
+                  >
+                    Register Now
+                  </button>
+                </form>
+
                 <p>
-                  I have read and accept to Exere{" "}
-                  <a href="#">Exere Terms & Conditions</a>
+                  Already have an account? <Link to="/login">Login</Link>
                 </p>
-              </div>
-            </div>
+              </section>
 
-            <br />
-            <button type="submit">Register Now</button>
-          </form>
+              <Link id="click_verify" to={`/verify_email?addr=${email}`}></Link>
 
-          <p>
-            Already have an account? <Link to="/login">Login</Link>
-          </p>
-        </section>
-
-        <Footer />
-      </>
+              <Footer />
+            </>
+          );
+        }}
+      </Loggeduser.Consumer>
     );
   }
 }
